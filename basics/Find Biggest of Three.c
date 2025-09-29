87f7caea1f1166e020b0b1c384a636e595419a02
Manir <maniruz.zaman.primary@gmail.com>
2025-09-29 11:27:51 +0530

    Create Find Biggest of Three.c

---
+++ basics/Find Biggest of Three.c
@@ -0,0 +1,16 @@
+//Write a C programe to find the biggest of given three numbers.
+
+#include<stdio.h>
+int main()
+{
+    int a, b, c, big;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    big = a;
+    if(b > big)
+        big = b;
+    if(c > big)
+        big = c;
+    printf("Biggest number is %d", big);
+    return 0;
+}
