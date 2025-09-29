cf1520da7b7f3b175fbd2d0afef48c2595e9d3fb
Manir <maniruz.zaman.primary@gmail.com>
2025-09-29 11:27:07 +0530

    Create Find Biggest of Two.c

---
+++ basics/Find Biggest of Two.c
@@ -0,0 +1,16 @@
+//Write a C programe to find the biggest of given two numbers.
+
+#include<stdio.h>
+int main()
+{
+    int a, b, big;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    big = a;
+    if(b>a) 
+    {
+        big = b;
+    }
+    printf("Biggest number is %d", big);
+    return 0;
+}
