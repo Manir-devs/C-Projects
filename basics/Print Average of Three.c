761db962f140f2c4065219c36c6ccf560ecae765
Manir <maniruz.zaman.primary@gmail.com>
2025-09-29 11:29:26 +0530

    Create Print Average of Three.c

---
+++ basics/Print Average of Three.c
@@ -0,0 +1,13 @@
+//Write a C programe code to print average of three numbers.
+
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    float avg;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    avg = (a + b + c) / 3.0;
+    printf("The average value of given three numbers is: %.2f", avg);
+    return 0;
+}
