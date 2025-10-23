766c88aa440097bffc271d6a290ff6ecd3f37382
Manir <maniruz.zaman.primary@gmail.com>
2025-10-23 22:59:53 +0530

    Create Find Simple Interest.c

---
+++ 2. Intermediate/Find Simple Interest.c
@@ -0,0 +1,15 @@
+//1 Find the simple interest of the following data - 
+//P = 5000, rate = 7.5, time = 4
+#include<stdio.h>
+
+int main() {
+    int p = 5000;        // principal
+    float rate = 7.5;   // rate of interest
+    int time = 4;       // time in years
+    float interest;
+
+    interest = p * time * rate / 100;
+    printf("Interest is %.2f\n", interest);
+
+    return 0;
+}
