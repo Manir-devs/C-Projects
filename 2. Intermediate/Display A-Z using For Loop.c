c44badd48236e03ff36c76ccbc279dd4b1ea758a
Manir <maniruz.zaman.primary@gmail.com>
2025-10-27 21:45:03 +0530

    Add program to display A-Z using for loop

---
+++ 2. Intermediate/Display A-Z using For Loop.c
@@ -0,0 +1,12 @@
+//4. Display A-Z using for loop. <file>4. Display A-Z using for loop</file>
+
+#include<stdio.h>
+int main()
+{
+  int i;
+  for(i=65; i<=90; i++);
+  {
+    printf("%c ", i);
+  }
+  return 0;
+}
