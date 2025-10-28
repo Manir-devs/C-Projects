3f5251c239f188e018cd8766da802d9e235e7fb2
Manir <maniruz.zaman.primary@gmail.com>
2025-10-28 13:27:43 +0530

    Update Find Biggest of Two.c

--- 1. Basics/Find Biggest of Two.c
+++ 1. Basics/Find Biggest of Two.c
@@ -1,4 +1,4 @@
-//2 Write a C programe to find the biggest of given two numbers.
+//2. Write a C program to find the biggest of given two numbers. <file>2. Write a C program to print the biggest of given two numbers</file><sm>https://manir-devs.github.io/C-Projects/Ignore-Me/code.html?path=1.%20Basics%2FFind%20Biggest%20of%20Two.c</sm>
 
 #include<stdio.h>
 int main()
