bf206ffcf65d95f2ed92c938a143227ca794fd8d
Manir <maniruz.zaman.primary@gmail.com>
2025-10-28 13:25:46 +0530

    Update Hello World.c

--- 1. Basics/Hello World.c
+++ 1. Basics/Hello World.c
@@ -1,4 +1,4 @@
-//1 Print Hello World!
+//1. Print Hello World! <file>1. Print hello world</file><sm>https://manir-devs.github.io/C-Projects/Ignore-Me/code.html?path=1.%20Basics%2FHello%20World.c</sm>
 
 #include<stdio.h>
 int main()
