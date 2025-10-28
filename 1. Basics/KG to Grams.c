9fb90ab10ffdbed3d8b99607b25287d1d4dfbfa4
Manir <maniruz.zaman.primary@gmail.com>
2025-10-28 13:32:16 +0530

    Update KG to Grams.c

--- 1. Basics/KG to Grams.c
+++ 1. Basics/KG to Grams.c
@@ -1,4 +1,4 @@
-//6 Write a C program to convert weight from kilograms to grams.
+//6. Write a C program to convert weight from kilograms to grams.<file>6. Write a C program to convert weight from kilograms to grams</file><sm>https://manir-devs.github.io/C-Projects/Ignore-Me/code.html?path=1.%20Basics%2FKG%20to%20Grams.c</sm>
 
 #include <stdio.h>
 int main() {
