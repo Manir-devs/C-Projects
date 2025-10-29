a01fd5bc1702edf5bdca9d8f8662bd4699202909
Manir <maniruz.zaman.primary@gmail.com>
2025-10-29 22:04:14 +0530

    Create Goto Statement.c

---
+++ 2. Intermediate/Goto Statement.c
@@ -0,0 +1,16 @@
+//8. Write a simple goto statement example. <file>8. Goto statement example</file>
+
+#include<stdio.h>
+int main()
+{
+  int i = 1;
+label1:
+  printf("%d ", i);
+  i++
+  if(i<=10)
+  {
+    goto label1;
+  }
+  printf("\nSuccessfully printed 1 to 10.");
+  return 0;
+}
