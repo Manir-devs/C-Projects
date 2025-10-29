a3120ab1dd420ea821a0ec2ce80d529c15c2684f
Manir <maniruz.zaman.primary@gmail.com>
2025-10-29 22:12:15 +0530

    Create Goto Pass or Fail.c

---
+++ 2. Intermediate/Goto Pass or Fail.c
@@ -0,0 +1,26 @@
+//9. Check pass or fail using goto statement. <file>9. Check pass or fail using goto statement</file>
+
+#include<stdio.h>
+int main()
+{
+  int num;
+  printf("Enter your mark: ");
+  scanf("%d", num);
+
+table1:
+  printf("Pass");
+  break;
+table2:
+  printf("Fail");
+  break;
+
+  if(num>30)
+  {
+    goto table1;
+  }
+  else
+  {
+    goto table2;
+  }
+  return 0;
+}
