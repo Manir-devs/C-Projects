88b28c0f5a4eb6c6fb351bf07fc26b1c5e14f5cd
Manir <maniruz.zaman.primary@gmail.com>
2025-10-29 22:28:21 +0530

    Update Goto Pass or Fail.c

--- 2. Intermediate/Goto Pass or Fail.c
+++ 2. Intermediate/Goto Pass or Fail.c
@@ -6,14 +6,15 @@ int main()
   int num;
   printf("Enter your mark: ");
   scanf("%d", &num);
+  printf("\nThen you are ");
   if(num>30)
     goto table1;
   else
     goto table2;
 table1:
-  printf("Pass");
+  printf("pass.");
   return 0;
 table2:
-  printf("Fail");
+  printf("fail!");
   return 0;
 }
