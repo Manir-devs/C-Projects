479992d7fa1bc1a25dff384cc5b27862bb78d8c3
Manir <maniruz.zaman.primary@gmail.com>
2025-10-29 21:01:52 +0530

    Add switch case example in C

---
+++ 2. Intermediate/Switch Case Example.c
@@ -0,0 +1,24 @@
+//7. Switch Case example. <file>7. Switch Case Example</file>
+
+#include<stdio.h>
+int main()
+{
+  int choice;
+  printf("Enter a number between 1 to 3: ");
+  scanf("%d", &choice);
+  switch(case)
+  {
+    case 1:
+      printf("You choosed 1.");
+      break;
+    case 2:
+      printf("You choosed 2.");
+      break;
+    case 3:
+      printf("You choosed 3.");
+      break;
+    default:
+      printf("Invalid choice");
+  }
+  return 0;
+}
