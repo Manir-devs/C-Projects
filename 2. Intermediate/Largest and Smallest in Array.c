fd0fe775d470cc83afbdf546e49a309045172269
Manir <maniruz.zaman.primary@gmail.com>
2025-11-12 00:01:09 +0530

    Create Largest and Smallest in Array.c

---
+++ 2. Intermediate/Largest and Smallest in Array.c
@@ -0,0 +1,32 @@
+//14. Find out the largest and smallest element in an array. <file>14. Find out the <mark>largest and smallest</mark> element in an <mark>array</mark></file>
+
+#include<stdio.h>
+int main()
+{
+	int i, n;
+	float a[50], large, small;
+	printf("Size of array values: ");
+	scanf("%d", &n);
+	printf("Array elements are: ");
+	for(i=0; i<n; i++)
+	{
+		scanf("%f", &a[i]);
+	}
+	large = a[0];
+	small = a[0];
+	for(i=1; i<n; i++)
+	{
+		if(a[i]>large)
+		{
+			large = a[i];
+		}
+		else if(a[i]<small)
+		{
+			small = a[i];
+		}
+	}
+	printf("Largest element in vector is %.2f", large);
+	printf("Smallest element in vector is %.2f", small);
+	return 0;
+}
+     
