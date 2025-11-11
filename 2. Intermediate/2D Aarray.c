96532928e92bfb5b1d217fc97e8b2caf7a8d11c9
Manir <maniruz.zaman.primary@gmail.com>
2025-11-12 00:21:25 +0530

    Create 2D Aarray.c

---
+++ 2. Intermediate/2D Aarray.c
@@ -0,0 +1,18 @@
+//15. Two dimensional array example. <file><mark>Two dimensional array</mark> example</file>
+
+#include<stdio.h>
+int main()
+{
+	int i, j;
+	int a[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+	printf("Elements of an array: \n\n");
+	for(i=0; i<3; i++)
+	{
+		for(j=0; j<3; j++)
+		{
+			printf("%d\t", a[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
