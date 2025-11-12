5611d3a703c8c6e9737d7fb5b31c38ccc059f378
Manir <maniruz.zaman.primary@gmail.com>
2025-11-12 19:16:34 +0530

    Create Function Example.c

---
+++ 3. Advanched/Function Example.c
@@ -0,0 +1,17 @@
+//1. Add 3 numbers using function. <file>1. Add 3 numbers using <mark>function</mark></file>
+
+#include<stdio.h>
+int add(int a, int b, int c)
+{
+        printf("\033[2J\033[H");
+        return a + b + c;
+}
+int main()
+{
+        printf("\033[2J\033[H");        //Clears the entire screen. 
+        int p,q,r,result;
+        printf("Enter 3 numbers: ");
+        scanf("%d %d %d", &p, &q, &r);
+        printf("Result is: %d", result);
+        return 0;
+}
