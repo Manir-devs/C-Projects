c499835f6616e2db584172975d7ea6ef98de99b9
Manir <maniruz.zaman.primary@gmail.com>
2025-11-12 19:19:52 +0530

    Update Function Example.c

--- 3. Advanched/Function Example.c
+++ 3. Advanched/Function Example.c
@@ -8,10 +8,11 @@ int add(int a, int b, int c)
 }
 int main()
 {
-        printf("\033[2J\033[H");        //Clears the entire screen. 
+        printf("\033[2J\033[H");             //Clears the entire screen.
         int p,q,r,result;
         printf("Enter 3 numbers: ");
         scanf("%d %d %d", &p, &q, &r);
+        result = add(p, q, r);               //p=a, q=b, r=c (from function add().)
         printf("Result is: %d", result);
         return 0;
 }
